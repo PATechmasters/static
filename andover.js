9cb37419c966bd3607b743b445f9a8642f727150
Tony Zhaocheng Tan <tony@tonytan.io>
2016-04-25 11:41:58 -0400

    modify andover.js

--- andover.js
+++ andover.js
@@ -2,11 +2,7 @@ var body = document.getElementsByTagName('body')[0];
 var mj = document.createElement("script");
 mj.type = "text/x-mathjax-config";
 mj[(window.opera ? "innerHTML" : "text")] =
-"MathJax.Hub.Config({\n"+
-" tex2jax:
-{ inlineMath: [['$','$'], ['\\\\(','\\\\)']], ignoreClass: \"tex2jax_ignore|answer_text|answer_comment_html\" }
-\n"+
-"});"
+"MathJax.Hub.Config({\n tex2jax:{ inlineMath: [['$','$'], ['\\\\(','\\\\)']], ignoreClass: \"tex2jax_ignore|answer_text|answer_comment_html\" }\n});"
 body.appendChild(mj);
 mj = document.createElement("script");
 mj.type = "text/javascript";
